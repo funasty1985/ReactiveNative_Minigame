551629a2ee4539bdd21a2ab52bd5a507c879f890
Fu <tony.f.liang@gmail.com>
2023-05-03 18:05:00 -0400

    update buttons

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StartGameScreen from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient
+      colors={["#4e0329", "#dd52f"]}
+      style={styles.rootScreen}
+    >
+      <StartGameScreen/>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  rootScreen: {
+    // backgroundColor: "#ddb52f",
+    flex: 1, // get up all the space of a screen
+  }
 });
